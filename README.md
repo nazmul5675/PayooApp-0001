e75b73efcdf282d8ac816ce1458a3d9d401a45fb
nazmul5675 <hridoyn61@gmail.com>
2025-09-01 04:22:28 +0600

    login interface working

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 "# PayooApp-0001" 
+live link "https://nazmul5675.github.io/PayooApp-0001/"
+
+for login use this,
+mobile number = "1000000000";
+Pin number = "1234";
---
+++ login.js
@@ -0,0 +1,22 @@
+// login btn to main page and if user enter wrong pin or mobile number show error on login page
+
+document.getElementById('login').addEventListener('click', function () {
+    const mobile = "1000000000";
+    const Pin = "1234";
+    const mobileInput = document.getElementById("mobile-number")
+    const mobileNumber = mobileInput.value;
+    const pinInput = document.getElementById('pin-number')
+    const pinNumber = pinInput.value;
+    if (mobileNumber === mobile && pinNumber === Pin) {
+        window.location.href = "main.html"
+    }
+    else {
+        console.log('show error');
+        const p = document.createElement('p')
+        p.textContent = " Invalid pin or mobile number"
+        p.classList.add('text-red', 'text-[10px]', 'underline', 'mx-auto');
+        document.getElementById("login-item-container").append(p)
+
+    }
+
+})
